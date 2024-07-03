c6a23bebb52c6088fe7952ac7c5f2b450d28c661
homeLinux <varenandrej@gmail.com>
2024-07-03 21:45:48 +0300

    header

--- Vector.cpp
+++ Vector.cpp
@@ -1,4 +1,6 @@
 #include "Vector.h"
 
 Vector::Vector(int s):elem{new double[s]},sz{s}{}
-
+Vector::~Vector(){delete elem;}
+double& Vector::operator[](int i){return elem[i];}
+int Vector::size(){return sz;}
--- user.cpp
+++ user.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "Vector.h"
 
+void sum(int s){
+    Vector v{s};
+    
+} 
+
 int main(){
-   
+   sum(3);
 } 
