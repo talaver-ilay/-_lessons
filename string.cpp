ab9ff19437c208e5a540036e7444419df9cb5b7a
homeLinux <varenandrej@gmail.com>
2024-07-04 22:03:53 +0300

    2:14:49

--- string.cpp
+++ string.cpp
@@ -1,26 +1,19 @@
 #include <iostream> 
 #include <cstring>
 int main(){
-    // std::string x{"Hallo world"};
-    // int count = 0;
-    // for(auto i: x){
-    //     ++count;
-    //     if(std::isblank(i)){
-    //         std::cout<<count<<"\n";
-    //     }
-    // } 
-    char* str1{"Hallo"};
-    char* str2 = "Hallo";
-    char* str3{"Hall"};
-    // if(std::strcmp(str1,str2)){
-    //     std::cout<<"equal"<<std::endl;
+    // char* str1{"Hallo"};
+    // char* str2 = "Hallo";
+    // char* str3{"Hall"};
+    // if(!std::strcmp(str1,str2)){
+    //     std::cout<<"str1 and str2 equal"<<std::endl;
     // }
-
-    if(std::strcmp(str1,str3)){
-        std::cout<<"equal"<<std::endl;
-    }else{
-        std::cout<<"not equal"<<std::endl;
-    }
+    // if(std::strcmp(str1,str3)){
+    //     std::cout<<"str1 and str3 not equal"<<std::endl;
+    // }
+    std::string str(4,'e');
+    std::cout<<str<<std::endl;
+    std::string str1{"HalloWorldd"};
+    std::string str2(str1,6,5);
+    std::cout<<str2<<std::endl;
     return 0;
-
 }
