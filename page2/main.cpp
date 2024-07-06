686459758e0ac4ec378b88535e82512da1513ed1
homeLinux <varenandrej@gmail.com>
2024-07-07 00:16:37 +0300

    5:50

---
+++ page2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "max.h"
+#include "min.h"
+
+int main(){
+    std::cout<<max(11,12)<<std::endl;
+    std::cout<<min(11,12)<<std::endl;
+    return 0;
+}
---
+++ page2/max.cpp
@@ -0,0 +1,6 @@
+#include "max.h"
+
+int max(int a, int b){
+    if(a>b) return a;
+    else return b;
+}
